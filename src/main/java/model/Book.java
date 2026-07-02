6023b891d36c98316a48fb0bffa8ab06c03c6fef
SamarthJ2005 <samjalkote111@gmail.com>
2026-07-02 20:11:37 +0530

    Complete Book model

---
+++ src/main/java/model/Book.java
@@ -0,0 +1,94 @@
+package model;
+
+public class Book {
+    private int id;
+    private String title;
+    private String author;
+    private String category;
+    private double price;
+    private int quantity;
+
+    public Book() {
+
+    }
+
+    // Constructor used while adding a new book
+    // MySQL will generate the id automatically
+    public Book(String title, String author, String category, double price, int quantity) {
+       this.title = title;
+       this.author = author;
+       this.category = category;
+       this.price = price;
+       this.quantity = quantity;
+    }
+
+    // Constructor used while reading a book from the database
+    public Book(int id, String title, String author, String category, double price, int quantity) {
+        this.id = id;
+        this.title = title;
+        this.author = author;
+        this.category = category;
+        this.price = price;
+        this.quantity = quantity;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getAuthor() {
+        return author;
+    }
+
+    public void setAuthor(String author) {
+        this.author = author;
+    }
+
+    public String getCategory() {
+        return category;
+    }
+
+    public void setCategory(String category) {
+        this.category = category;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public void setPrice(double price) {
+        this.price = price;
+    }
+
+    public int getQuantity() {
+        return quantity;
+    }
+
+    public void setQuantity(int quantity) {
+        this.quantity = quantity;
+    }
+
+    @Override
+    public String toString() {
+        return "Book{" +
+                "id=" + id +
+                ", title='" + title + '\'' +
+                ", author='" + author + '\'' +
+                ", category='" + category + '\'' +
+                ", price=" + price +
+                ", quantity=" + quantity +
+                '}';
+    }
+}
